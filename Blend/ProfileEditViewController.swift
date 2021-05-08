eddc2583292b78d9e38cc49b07f339ef578d114e
Tahmid Zaman <tahmid198@yahoo.com>
2021-05-07 22:22:05 -0400

    Edit profile and Preference view control

---
+++ Blend/ProfileEditViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileEditViewController.swift
+//  Blend
+//
+//  Created by Tahmid Zaman on 5/7/21.
+//
+
+import UIKit
+
+class ProfileEditViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
